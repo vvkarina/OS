86e20fb03c61fd68671cf891efaef9723e95f8d5
vvkarina <v0karina0v@gmail.com>
2022-12-22 21:43:52 +0300

    Add folders Lab4

--- lab4/src/parent.cpp
+++ lab4/src/parent.cpp
@@ -18,6 +18,7 @@
 #include <signal.h>
 #include <fstream>
 #include <memory>
+sem_t *sem1, *sem2, *sem3, *sem4;
 
 std::vector<std::string> ParentRoutine(const std::vector<std::string> &input)
 {
@@ -46,11 +47,10 @@ std::vector<std::string> ParentRoutine(const std::vector<std::string> &input)
     {
         Oerror("ftruncate", -1);
     }
-    sem_t *sem1 = sem_open("1", O_CREAT, S_IRUSR | S_IWUSR, 0);
-    sem_t *sem2 = sem_open("2", O_CREAT, S_IRUSR | S_IWUSR, 1);
-    sem_t *sem3 = sem_open("3", O_CREAT, S_IRUSR | S_IWUSR, 0);
-    sem_t *sem4 = sem_open("4", O_CREAT, S_IRUSR | S_IWUSR, 1);
-
+    sem1 = sem_open("1", O_CREAT | O_EXCL, 0777, 0);
+    sem2 = sem_open("2", O_CREAT | O_EXCL, 0777, 0);
+    sem3 = sem_open("3", O_CREAT | O_EXCL, 0777, 0);
+    sem4 = sem_open("4", O_CREAT | O_EXCL, 0777, 0);
     pid_t pid1;
     pid_t pid2;
     pid1 = fork();
@@ -60,46 +60,54 @@ std::vector<std::string> ParentRoutine(const std::vector<std::string> &input)
     }
     if (pid1 == 0)
     {
-
         char *in = (char *)mmap(NULL, SIZE, PROT_READ, MAP_SHARED, file1, 0);
         char *ans = (char *)mmap(NULL, SIZE, PROT_WRITE, MAP_SHARED, file3, 0);
-        sem_wait(sem1);
-        for (int i = 0; i < SIZE; ++i)
+        sem1 = sem_open("1", 0);
+        sem2 = sem_open("2", 0);
+        while (1)
         {
-            if (in[i] >= 'A' && in[i] <= 'Z')
+            sem_wait(sem1);
+            for (int i = 0; i < SIZE; ++i)
             {
-                ans[i] = std::tolower(in[i]);
+                if (in[i] >= 'A' && in[i] <= 'Z')
+                {
+                    ans[i] = std::tolower(in[i]);
+                }
+                else
+                {
+                    ans[i] = in[i];
+                }
             }
-            else
-            {
-                ans[i] = in[i];
-            }
+            munmap(in, SIZE);
+            munmap(ans, SIZE);
+            sem_post(sem2);
         }
-        munmap(in, SIZE);
-        munmap(ans, SIZE);
-        sem_post(sem2);
     }
     if (pid1 > 0 && pid2 == 0)
     {
         char *in = (char *)mmap(NULL, SIZE, PROT_READ, MAP_SHARED, file3, 0);
         char *ans = (char *)mmap(NULL, SIZE, PROT_WRITE, MAP_SHARED, file2, 0);
-        sem_wait(sem3);
-
-        for (int i = 0; i < SIZE; ++i)
+        sem3 = sem_open("3", 0);
+        sem4 = sem_open("4", 0);
+        while (1)
         {
-            if (in[i] == ' ')
-            {
-                ans[i] = '_';
-            }
-            else
-            {
-                ans[i] = in[i];
-            }
-        }
+            sem_wait(sem3);
 
-        munmap(in, SIZE);
-        munmap(ans, SIZE);
-        sem_post(sem4);
+            for (int i = 0; i < SIZE; ++i)
+            {
+                if (in[i] == ' ')
+                {
+                    ans[i] = '_';
+                }
+                else
+                {
+                    ans[i] = in[i];
+                }
+            }
+            munmap(in, SIZE);
+            munmap(ans, SIZE);
+            sem_post(sem4);
+        }
     }
     if (pid1 == -1 || pid2 == -1)
     {
@@ -109,11 +117,16 @@ std::vector<std::string> ParentRoutine(const std::vector<std::string> &input)
     if (pid1 != 0 && pid2 != 0)
     {
         char *ans1 = (char *)mmap(NULL, SIZE, PROT_WRITE, MAP_SHARED, file2, 0);
+        sem1 = sem_open("1", 0);
+        sem2 = sem_open("2", 0);
+        sem3 = sem_open("3", 0);
+        sem4 = sem_open("4", 0);
         sem_post(sem1);
         sem_wait(sem2);
         str.clear();
         sem_post(sem3);
         sem_wait(sem4);
+
         for (int i = 0; i < SIZE; ++i)
         {
             readChar = ans1[i];
@@ -128,15 +141,16 @@ std::vector<std::string> ParentRoutine(const std::vector<std::string> &input)
             }
         }
         munmap(ans1, SIZE);
+        sem_close(sem1);
+        sem_close(sem2);
+        sem_close(sem3);
+        sem_close(sem4);
+        sem_unlink("1");
+        sem_unlink("2");
+        sem_unlink("3");
+        sem_unlink("4");
+        kill(pid1, SIGKILL);
+        kill(pid2, SIGKILL);
     }
-    wait(NULL);
-    sem_close(sem1);
-    sem_close(sem2);
-    sem_close(sem3);
-    sem_close(sem4);
-    sem_unlink("1");
-    sem_unlink("2");
-    sem_unlink("3");
-    sem_unlink("4");
     return output;
 }
